deb229f10685b0a4b5b716e38038353d027f26ac
TheOne28 <vincentpatmadja@gmail.com>
2021-08-13 11:24:33 +0700

    Deleting search html

--- src/pages/sign_up.js
+++ src/pages/sign_up.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import styles from './login.module.css'
-import {Link} from 'react-router-dom'
-
-function LoginPage() {
-    return(
-        <div className={styles.loginpage}>
-            <div className={styles.form}>
-                <div className={styles.login}>
-                    <h3>SIGN UP</h3>
-                </div>
-                <form className = {styles.form}>
-                    <input type="email" placeholder="email"/>
-                    <input type="password" placeholder="password"/>
-                    <input type="password" placeholder="password confirmation"/>
-                    <button>sign up</button>
-                    <p className={styles.message}>Not registered? <a href="#"><Link to = "/signup" >Sign Up</Link></a></p>
-=======
 import styles from './sign_up.module.css'
 import {Link} from 'react-router-dom'
 
@@ -35,7 +17,6 @@ function SignUpPage() {
                     <input type="password" placeholder="password confirmation"/>
                     <button>Sign Up</button>
                     <p class="message">Registered ? <Link to="/login">Log in </Link></p>
->>>>>>> 81b8e4a6d6297edd77cf5f956819fd0dd75cd26c
                 </form>
             </div>
         </div>
